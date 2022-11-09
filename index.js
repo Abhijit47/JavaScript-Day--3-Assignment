07e522ac99bf0d00872adbcad1a44751fd316d30
Abhijit47 <akarmakar846@gmail.com>
2022-11-09 22:04:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// Write a statement to find the largest of five numbers. Display an alert box to show the result
+(a = 6), (b = 9), (c = 7), (d = 3), (e = 1);
+
+if (a > b && a > c && a > d && a > e) {
+  // console.log("a is the greatest");
+  alert("a is the greatest");
+} else if (b > a && b > c && b > d && b > e) {
+  // console.log("b is the greatest");
+  alert("b is the greatest");
+} else if (c > a && c > b && c > d && c > e) {
+  // console.log("c is the greatest");
+  alert("c is the greatest");
+} else if (d > a && d > b && d > c && d > e) {
+  // console.log("d is the greatest");
+  alert("d is the greatest");
+} else {
+  // console.log("e is the greatest");
+  alert("e is the greatest");
+}
